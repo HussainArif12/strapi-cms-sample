b77608e5cd07f2939c3357bf6c8ac5329b421908
HussainArif <hussainarifkl@gmail.com>
2021-02-25 23:19:30 +0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd1b58e82b01a5ee70cd98495663a205b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'aed296e5-bda9-4807-b5f4-3f3df388198d'
+};
