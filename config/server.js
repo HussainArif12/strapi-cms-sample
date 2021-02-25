edc4c9a7e562c420090878d22c89a9db7aa87618
HussainArif <hussainarifkl@gmail.com>
2021-02-25 23:53:10 +0500

    Fixed mistake

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("HEROKU_URL"),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'd1b58e82b01a5ee70cd98495663a205b'),
+      secret: env("ADMIN_JWT_SECRET", "d1b58e82b01a5ee70cd98495663a205b"),
     },
   },
 });
